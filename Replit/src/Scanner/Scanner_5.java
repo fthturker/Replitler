874d55c25108ce8ad808341e2b34490805d7d192
fturker80 <fthturker80@gmail.com>
2022-04-14 17:32:21 +0300

    14.04.2022

---
+++ Replit/src/Scanner/Scanner_5.java
@@ -0,0 +1,19 @@
+package Scanner;
+
+import java.util.Scanner;
+
+public class Scanner_5 {
+    /*
+    int veri türünün Maximum ve Minimum değerlerini gösteren kodu konsola yazınız.
+     */
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        int sayi=scan.nextInt();
+
+
+
+    }
+}
