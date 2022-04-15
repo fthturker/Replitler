ed998847dec64ae573ee79ba26e7bbf96ecda0c1
fturker80 <fthturker80@gmail.com>
2022-04-15 18:57:46 +0300

    15.04.2022

--- OCA/src/Soru_18_ForLoopContinueKullanimi.java
+++ OCA/src/Soru_18_ForLoopContinueKullanimi.java
@@ -2,11 +2,12 @@ public class Soru_18_ForLoopContinueKullanimi {
 
     public static void main(String[] args) {
 
-        int count =0;
-        ROW_LOOP: for (int row=1; row <=3; row++)
-            for(int col=1; col<=2; col++) {
-            if (row * col % 2 ==0) continue ROW_LOOP;
-            count++;
+        int count = 0;
+        ROW_LOOP:
+        for (int row = 1; row <= 3; row++)
+            for (int col = 1; col <= 2; col++) {
+                if (row * col % 2 == 0) continue ROW_LOOP;
+                count++;
             }
         System.out.println(count);
     }
