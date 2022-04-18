6385d72176dfc1033cc357b9e72577890d729b2e
fturker80 <fthturker80@gmail.com>
2022-04-18 21:44:40 +0300

    18.04.2022

--- Java2022Winter/src/day00_denemeler/C03.java
+++ Java2022Winter/src/day00_denemeler/C03.java
@@ -1,21 +1,18 @@
 package day00_denemeler;
 
+import java.util.LinkedList;
+
 public class C03 {
     public static void main(String[] args) {
- /* TASK :
-          Elinizde bir miktar farklı boylarda somun ve bu somunlara uyacak civatalar olsun.
-          Civataları somunlara eşlemek için nasıl bir algoritma yazarsınız.
-        */
-        String[] somun={"m7","m20","m6","m10","m12","m5","m11"};
-        String[] civata={"m5","m8","m11","m20","m7","m9","m10"};
+        LinkedList<Integer> list=new LinkedList<>();
+        list.add(5);
+        list.add(10);
+        list.addFirst(11);
+        list.addLast(20);
 
-        for (int i = 0; i < somun.length; i++) {
-            for (int j = 0; j < civata.length; j++) {
-                if (civata[j].contains(somun[i])){
-                    System.out.println(civata[j]+"=civata ile " + ", " +somun[i]+ " =somun ile uyumludur");
-                }
-            }
-        }
+        System.out.println(list);
+        list.add(0,70);
+        System.out.println(list);
 
 
         }
