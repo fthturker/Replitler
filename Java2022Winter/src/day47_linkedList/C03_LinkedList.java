082208ce1b3dd1e7b079457aef79880ca4b1bb8c
fturker80 <fthturker80@gmail.com>
2022-04-18 20:45:36 +0300

    18.04.2022

--- Java2022Winter/src/day47_linkedList/C03_LinkedList.java
+++ Java2022Winter/src/day47_linkedList/C03_LinkedList.java
@@ -8,7 +8,7 @@ public class C03_LinkedList {
     public static void main(String[] args) {
 
 
-        Deque<Integer> ll1=new LinkedList<>();
+        Deque<Integer> ll1 = new LinkedList<>();
         System.out.println(ll1.peek()); // bosken kullanrsak null dondurur
 
         //System.out.println(ll1.element()); // bosken kullanirsak exception verir
@@ -18,10 +18,22 @@ public class C03_LinkedList {
         System.out.println(ll1.pollLast()); //
 
         // ll1.pop(); // ilk elementi silip bize dondurur
-                      // ilk elementi bulamazsa exception firlatir
+        // ilk elementi bulamazsa exception firlatir
 
+        ll1.push(30); // Listenin basina element ekler
+        ll1.push(20);
+        System.out.println(ll1);// [20, 30]
+
+        ll1.remove(); // ilk elementi siler ve bize dondurur // 20
+
+        System.out.println(ll1.remove());
+        ll1.push(40);
+        ll1.push(50);
+        ll1.remove(50); // object olarak 50 yi siler
         ll1.push(30);
-
+        System.out.println(ll1);
+        ll1.removeFirstOccurrence(30);
+        System.out.println(ll1);
 
     }
 }
---
+++ Java2022Winter/src/day47_linkedList/C04_ArraydenTekrarlariSilme.java
@@ -0,0 +1,40 @@
+package day47_linkedList;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class C04_ArraydenTekrarlariSilme {
+    public static void main(String[] args) {
+
+        // verilen bir array'den tekrar eden elementleri silip
+        //unit elementlerden olusan bir array haline donduren bir method yaziniz
+        Integer arr[] = {3, 5, 4, 6, 3, 1, 2, 7, 8, 6, 1, 4, 2, 8};
+
+        arr = benzersizYap(arr);
+        System.out.println(Arrays.toString(arr)); // [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+    }
+
+    private static Integer[] benzersizYap(Integer[] arr) {
+        Set<Integer> benzersizSet=new TreeSet<>();
+        for (Integer each:arr
+             ) {
+            benzersizSet.add(each);
+        }
+        System.out.println(benzersizSet); // [1, 2, 3, 4, 5, 6, 7, 8]
+        // Set'de index yapisi yoktur. dolayisiyla for loop kullanmayiz
+
+        Integer arrBenzersiz[]=new Integer[benzersizSet.size()];
+        int index=0;
+        for (Integer each:benzersizSet
+             ) {
+            arrBenzersiz[index]=each;
+            index++;
+        }
+
+        return arrBenzersiz;
+    }
+}
