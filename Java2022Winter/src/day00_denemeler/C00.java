e94e34bf6d00827091731ad8bcae332f3d951d22
fturker80 <fthturker80@gmail.com>
2022-04-19 16:38:21 +0300

    19.04.2022

--- Java2022Winter/src/day00_denemeler/C00.java
+++ Java2022Winter/src/day00_denemeler/C00.java
@@ -1,33 +1,77 @@
 package day00_denemeler;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class C00 {
     public static void main(String[] args) {
-        /* TASK :
-          Elinizde bir miktar farklı boylarda somun ve bu somunlara uyacak civatalar olsun.
-          Civataları somunlara eşlemek için nasıl bir algoritma yazarsınız.
-        */
+        /*
+    Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array.
 
-        int [] arrSomun= {99,3,9,1,7,5,};
-        int [] arrCivata= {1,5,3,9,7,99};
+    Note: the built-in Math.min(v1, v2) and Math.max(v1, v2) methods return the smaller or larger of two values.
 
-        dizinYap(arrSomun);
-        dizinYap(arrCivata);
-        for (int i = 0; i < arrCivata.length; i++) {
-            System.out.println("arrCivata : "+arrCivata[i] + " = arrSomun : " +arrSomun[i]);
-        }
-    }
 
-    private static int[] dizinYap(int[] arrA) {
-        for (int i = 0; i < arrA.length; i++) {
-            for (int j = 0; j < arrA.length; j++) {
-                if (arrA[i]<arrA[j]){
-                    int ab=arrA[i];
-                    arrA[i]=arrA[j];
-                    arrA[j]=ab;
-                }
+    bigDiff([10, 3, 5, 6]) → 10-3 result = 7
+    bigDiff([7, 2, 10, 9]) → 10-2 result = 8
+    bigDiff([2, 10, 7, 1]) → 10-1 result = 9
+
+    return type is int
+     */
+
+     /*
+
+      Bir veya daha fazla öğe iceren int array verildiğinde, arraydeki en büyük ve en küçük öğeler arasındaki farkı return edin.
+      Not: the built-in    Math.min(v1, v2) ve   Math.max(v1, v2) methodları en küçük ve en büyük öğeleri return eder.
+
+
+    bigDiff([10, 3, 5, 6]) → 10-3 result = 7
+    bigDiff([7, 2, 10, 9]) → 10-2 result = 8
+    bigDiff([2, 10, 7, 1]) → 10-1 result = 9
+
+    return tipi  int dir
+     */
+
+
+
+            Scanner scan = new Scanner(System.in);
+
+            String myStr = scan.nextLine();
+
+            String[] strArr = myStr.split(" ");
+
+            int[] useThisArray = new int[strArr.length];
+
+        Arrays.sort(useThisArray);
+        int min = useThisArray [0];
+        int max = useThisArray[useThisArray.length -1];
+            for (int i = 0; i < strArr.length; i++) {
+
+
+                int num = Integer.parseInt(strArr[i]);
+                useThisArray[i] = num;
+
+                System.out.println(max - min);
+            /*
+            int[] useThisArray = {2, 4, 3, 5, 9,};
+            Arrays.sort(useThisArray);
+            int min = useThisArray [0];
+            int max = useThisArray[useThisArray.length -1];
+            System.out.println("Min Value = " + min);
+            System.out.println("Max Value = " + max);
+            System.out.println(max-min);
+             */
             }
+
+            // Your code start here. Don't remove or change anything before this line. Your array is -> useThisArray in line 27
+            // Kodlamaya burdan başla.Bu satırdan önceki satırlarda hiçbirşeyi kaldırma ve degiştirme. Kullanacağın array 27.ci satırda  --> useThisArray
+
+
         }
-        return arrA;
+
     }
 
-}
+
+
+
+
+
